4a6107f021224f6043494879b5e2f6991bb8ef16
ahmed <ahmd66699@gmail.com>
2022-02-20 18:51:49 +0300

    Set up project

---
+++ page.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>You sign in</title>
+</head>
+<body>
+    <?php
+        echo "User Name : ".$_POST["userName"]."<br>"."User Pcleassword :".$_POST["userPassword"];
+    ?>
+</body>
+</html>
